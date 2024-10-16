35b734fc83dcb4994d2a20c1f63fe2687ed70a5d
ayoubleass <ayoubleas@gmail.com>
2024-10-16 14:42:36 +0000

    Create :file 0-uniq_users.sql, 1-country_users.sql, 2-fans.sql, 3-glam_rock.sql

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,3 @@
+-- This SQL script lists all bands with Glam rock as their main style, ranked by their longevity.
+SELECT band_name, COALESCE(split, 2022) - formed AS lifespan FROM metal_bands
+WHERE style="Glam rock" ORDER BY lifespan DESC;
