9bda4d2d1f0fc86ccc9e109e621394c7b485ffee
ayoubleass <ayoubleas@gmail.com>
2024-10-17 11:00:32 +0000

    CREATE FILE: 6-bonus.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- SQL script that creates a table users
-
+-- If table exists, script will not fail, can be executed on any database
 CREATE TABLE IF NOT EXISTS users (
        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
        email VARCHAR(255) NOT NULL UNIQUE,
