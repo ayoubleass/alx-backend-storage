011cc5bb4ffd72a33415d23c5951f0dd0d0ec91c
ayoubleass <ayoubleas@gmail.com>
2024-10-17 11:17:23 +0000

    Update FILE: 6-bonus.sql

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -2,10 +2,12 @@
 DELIMITER //
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name varchar(255), IN score INT)
 BEGIN
-	IF NOT EXISTS(SELECT 1 FROM  projects WHERE name = project_name)THEN
+	DECLARE project_id INT;
+	IF NOT EXISTS(SELECT 1 FROM  projects WHERE name = project_name) THEN
 		INSERT INTO projects (name) VALUES (project_name);
 	END IF;
-	INSERT INTO corrections (user_id, project_name, score)
-	VALUES (user_id,(SELECT id FROM projects where name = project_name), score);
+	SELECT id INTO project_id FROM projects where name = project_name;
+	INSERT INTO corrections (user_id, project_id, score)
+	VALUES (user_id, project_id, score);
 END //
 DELIMITER;
